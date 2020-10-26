803bd8a4838627c9dcf55108ef964fafd296558e
BrammBosch <31956479+BrammBosch@users.noreply.github.com>
2020-10-26 10:29:21 +0100

    Improved script so it won't pull excluded events

--- LMT/scripts/tools/select_events.py
+++ LMT/scripts/tools/select_events.py
@@ -1,13 +1,30 @@
 import sqlite3
+import sys
 
-def connection(table):
-    conn = sqlite3.connect(
-        table)  # <- Connect to the database using the variable declared in main
+
+def connection(table, *args):
+    conn = sqlite3.connect(table)  # <- Connect to the database using the variable declared in main
 
     cursor = conn.cursor()
 
-    cursor.execute("select * from event")
+    # SQL_Query = pd.read_sql("select * from event", conn)
+    # df = pd.DataFrame(SQL_Query)
+
+    if args != ():
+        list_excluded_events = args[0]
+
+        placeholder = '?'
+        placeholders = ', '.join(placeholder for unused in list_excluded_events)
+
+        query = "select * from event where NAME NOT IN (%s) limit 10000" % placeholders
+
+        cursor.execute(query, list_excluded_events)
+
+    else:
+        cursor.execute("select * from event")
 
     results = cursor.fetchall()
-    results = [list(elem) for elem in results]  # <- Change list of tuples to a list of lists
+    #print('The lenght of the results in bytes = ' + str(sys.getsizeof(results)))
+
+    #results = [list(elem) for elem in results]  # <- Change list of tuples to a list of lists
     return results
