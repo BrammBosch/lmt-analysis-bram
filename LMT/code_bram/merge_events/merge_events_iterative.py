c53ec75697d461d1b672ad7efd9ab90677482831
BrammBosch <31956479+BrammBosch@users.noreply.github.com>
2020-11-10 13:59:45 +0100

    Added documentation

--- LMT/code_bram/merge_events/merge_events_iterative.py
+++ LMT/code_bram/merge_events/merge_events_iterative.py
@@ -1,14 +1,19 @@
 import sqlite3
 import time
 from multiprocessing import Process, Manager
+
+from scripts.tools.find_time_frames import find_start_end_file
 from scripts.tools.select_db import connection
-from scripts.tools.sort_split import sort_split
 from lmtanalysis.FileUtil import getFilesToProcess
 
 animalResults = []
 
 
 def main_event_merge(table):
+    """
+    If this function is called with a file location it will replace all events with their merged versions.
+    """
+
     frames = 15  # <- The amount of frames between events based on which the merge is performed
     listExcludedEvents = ['RFID ASSIGN ANONYMOUS TRACK',
                           'RFID MATCH',
@@ -18,18 +23,18 @@ def main_event_merge(table):
                           'Head detected'
                           ]  # <- All of these events are not looked at to merge
     start = time.time()
+    start_frame, end_frame = find_start_end_file(table)
 
-    results = connection(table, listExcludedEvents)  # <- Here the even table is pulled
+    results = connection(table, listExcludedEvents,start_frame,end_frame)  # <- Here the even table is pulled
     print('The length of the entire table = ' + str(len(results)))
-    #results = sort_split(results)  # <- This takes the event table (2d list) as input and returns a 3d list sorted for each animal
-    # i = 0
-    # for animal in results:
-    #     print("The length of animal " + str(i + 1) + " before merging = " + str(len(animal)))
-    #     i += 1
-    # print("-" * 15)
+
 
     animals = []
     unique_event = []
+
+    """
+        This for loop sorts all events based on their name
+    """
 
     for line in results:
         if line[1] not in unique_event:
@@ -38,13 +43,22 @@ def main_event_merge(table):
 
         animals[unique_event.index(line[1])].append(line)
 
+
+    """
+        This loop sorts each list of events based on the animals and the start frames
+    """
     it = 0
+
     for animal in animals:
         animals[it] = sorted(animals[it], key=lambda x: (x[5], (x[6] is None, x[6]), (x[7] is None, x[7]), (x[8] is None, x[8]),
                                                    x[1], x[3]))
         it+= 1
 
 
+    """
+    This loops over all of the events and calls the merging script.
+    It uses a pool manager to multiprocess the scripts to optimise the program.
+    """
     with Manager() as manager:
         animalResults = manager.list()  # <- declare a special list variable to be used by multiple processes
 
@@ -57,24 +71,25 @@ def main_event_merge(table):
             p.join()
         animalResults = list(animalResults)
 
-    i = 0
     totalLen = 0
     for result in animalResults:
-    #     print('The length of animal ' + str(i + 1) + ' after merging = ' + str(len(result)))
         totalLen += len(result)
-    #     i += 1
     print('The length of the entire table after merging = ' + str(totalLen))
 
-    #replace_table(table, animalResults)  # <- This calls the function to replace the event table with the new merged events.
+    replace_table(table, animalResults)  # <- This calls the function to replace the event table with the new merged events.
     end = time.time()
     print('time elapsed: ' + str(end - start))
 
 
 def replace_table(table, animalResults):
+    """
+    This functions deletes the existing event table and completely replaces it with the new merged events table
+    """
     conn = sqlite3.connect(table)  # <- Connect to the database using the variable declared in main
     cursor = conn.cursor()
+    print('Replacing')
     cursor.execute(
-        'DELETE FROM event')  # <- Delete the entire event table. This is faster than looking at each row to see where changes happened
+        'DELETE FROM event')
     for animal in animalResults:
         for row in animal:
             sql = "INSERT INTO event VALUES (?,?,?,?,?,?,?,?,?,?)"  # <- Replace the event table row by row with the new merged events.
@@ -85,10 +100,15 @@ def replace_table(table, animalResults):
 
 
 def check_events(L, result, frames):
+    """
+    This function is where each row is compared to the row after is to see if they should be merged.
+    """
     try:
         k = 0
+        listTime = []
         for row in result:
-            #print(row)
+            start = time.time()
+
             if result.index(row) + 1 == len(result):
                 raise IndexError  # <- If the row that is checked it the last row it raises and catches an error to end the loop
 
@@ -98,12 +118,6 @@ def check_events(L, result, frames):
                 4] < frames:  # <- If the event name is the same as the next row and the animals are the same as the next row
                 # and the frame difference is less than the variable called in the main
                 same = True
-                # print(row[4])
-                # print(nextLine[3])
-                # print(nextLine[3] - row[4])
-                #
-                # print('merging with:')
-                # print(nextLine)
 
                 result[result.index(row)][4] = nextLine[4]
 
@@ -115,16 +129,20 @@ def check_events(L, result, frames):
                     if row[1] == nextLine[1] and row[5:] == nextLine[5:] and nextLine[3] - row[
                         4] < frames:  # <- checks the same things as the previous if statements to see if there are more than 1 similar rows
                         result[result.index(row)][4] = nextLine[4]
-                        # print('merging with:')
-                        # print(nextLine)
+
                         result.remove(nextLine)
                     else:
                         same = False
             k += 1
-            #print(' ')
 
+            end = time.time()
+            listTime.append(end - start)
 
     except IndexError:
+        print(result[0][1])
+        print('avg time ' + str(sum(listTime)/len(listTime)))
+        print('len ' + str(len(listTime)))
+        print('-'*15)
         L.append(result)
 
 
