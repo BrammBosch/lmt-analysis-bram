a8c1dd1ffee91c66485defdd3791064356914e42
BrammBosch <31956479+BrammBosch@users.noreply.github.com>
2020-12-17 14:42:12 +0100

    Wrote new function to remove overlapping time intervals

--- LMT/code_bram/quality/check_match_mismatch.py
+++ LMT/code_bram/quality/check_match_mismatch.py
@@ -1,4 +1,4 @@
-from scripts.tools.sort_split import sort_split
+from scripts.tools.find_time_frames import find_start_end_file
 from scripts.tools.select_db import connection_match
 
 
@@ -6,12 +6,13 @@ def match_mismatch_main(table):
     result = connection_match(table)
 
     count_match, count_mismatch = count_match_mismatch(result)
-    time_mismatch_last_match = time_mismatch_match(result)
-    #print(time_mismatch_last_match)
-    print('The amount of matches for mice 1,2,3 and 4 are: ' + str(count_match))
-    print('The amount of mismatches for mice 1,2,3 and 4 are: ' + str(count_mismatch))
+    time_mismatch_last_match = time_mismatch_match_no_double(result)
+    # print(time_mismatch_last_match)
+    # print('The amount of matches for mice 1,2,3 and 4 are: ' + str(count_match))
+    # print('The amount of mismatches for mice 1,2,3 and 4 are: ' + str(count_mismatch))
 
-    return count_match,count_mismatch,time_mismatch_last_match
+    return count_match, count_mismatch, time_mismatch_last_match
+
 
 def count_match_mismatch(result):
     match_and_mismatch = []
@@ -39,10 +40,10 @@ def count_match_mismatch(result):
                 count_mismatch[3] += 1
             match_and_mismatch.append(row)
 
-
     return count_match, count_mismatch
 
-def time_mismatch_match(result):
+
+def time_mismatch_match_doubles(result):
     time_mismatch_last_match = [[], [], [], []]
     tempMatch1 = 0
     tempMatch2 = 0
@@ -63,7 +64,7 @@ def time_mismatch_match(result):
                 tempMatch4 = row[3]
         if row[1] == 'RFID MISMATCH':
             if row[5] == 1:
-                #print('time between is: ' + str(row[3]) + str(' ') + str(tempMatch1) + ' for mouse 1')
+                # print('time between is: ' + str(row[3]) + str(' ') + str(tempMatch1) + ' for mouse 1')
                 time_mismatch_last_match[0].append(row[3] - tempMatch1)
 
             elif row[5] == 2:
@@ -79,6 +80,92 @@ def time_mismatch_match(result):
                 time_mismatch_last_match[3].append(row[3] - tempMatch4)
 
     return time_mismatch_last_match
+
+
+def time_mismatch_match_no_double(result):
+    time_mismatch_last_match = [[], [], [], []]
+    tempMatch1 = 0
+    tempMatch2 = 0
+    tempMatch3 = 0
+    tempMatch4 = 0
+    last_match = [[], [], [], []]
+
+    for row in result:
+        if row[1] == 'RFID MATCH':
+            if row[5] == 1:
+                tempMatch1 = row[3]
+            elif row[5] == 2:
+                tempMatch2 = row[3]
+            elif row[5] == 3:
+                tempMatch3 = row[3]
+            else:
+                tempMatch4 = row[3]
+
+        if row[1] == 'RFID MISMATCH':
+
+            if row[5] == 1:
+                print('time between is: ' + str(row[3]) + str(' ') + str(tempMatch1) + ' for mouse 1')
+                print(row[3]-tempMatch1)
+
+                last_match[0].append(tempMatch1)
+                time_mismatch_last_match[0].append(row[3] - tempMatch1)
+
+            elif row[5] == 2:
+                last_match[1].append(tempMatch2)
+                time_mismatch_last_match[1].append(row[3] - tempMatch2)
+
+            elif row[5] == 3:
+                last_match[2].append(tempMatch3)
+                time_mismatch_last_match[2].append(row[3] - tempMatch3)
+
+            elif row[5] == 4:
+                last_match[3].append(tempMatch4)
+                time_mismatch_last_match[3].append(row[3] - tempMatch4)
+
+        # if row[5] == 1:
+        #     #print(row)
+        #     tempMatch1 = row[3]
+        # elif row[5] == 2:
+        #     tempMatch2 = row[3]
+        # elif row[5] == 3:
+        #     tempMatch3 = row[3]
+        # else:
+        #     tempMatch4 = row[3]
+
+
+    list_same = [[], [], [], []]
+
+    k = 0
+    for animal in last_match:
+        temp = 0
+        i = 0
+        for line in animal:
+            if line == temp:
+                list_same[k].append(i - 1)
+            i += 1
+            temp = line
+        k += 1
+
+    k = 0
+    for same_list in list_same:
+        for index in sorted(same_list, reverse=True):
+            del time_mismatch_last_match[k][index]
+        k += 1
+
+    return time_mismatch_last_match
+
+
 if __name__ == '__main__':
-    table = 'C:/Users/Bram/Documents/radboud/LMT_data_original/28042020_20170048001_Group2_PreTreatment.sqlite'
-    match_mismatch_main(table)
+    table = 'D:\LMT_data_post/25082020_20170048002_Group2_PreTreatment.sqlite'
+    a, b, c = match_mismatch_main(table)
+    start_frame, end_frame = find_start_end_file(table)
+    total_time = end_frame - start_frame
+    print('amount of matches ' + str(a))
+    print('amount of mismatches ' + str(b))
+    print(len(c[0]))
+    print(c)
+    print(sum(c[0]))
+    print('total time: ' + str(total_time) + ' mismatch time: ' + str(sum(c[0])))
+    print('percentage mismatch time ' + str((sum(c[0]) / total_time) * 100))
+
+    # print(c[0])
