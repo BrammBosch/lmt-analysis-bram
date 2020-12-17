306d07cdd354544598a539b34cd311463ef10aca
BrammBosch <31956479+BrammBosch@users.noreply.github.com>
2020-12-17 14:49:03 +0100

    Added documentation and removed redundant function

--- LMT/scripts/tools/read_db_info.py
+++ LMT/scripts/tools/read_db_info.py
@@ -2,19 +2,32 @@ import pandas as pd
 from lmtanalysis.FileUtil import getFilesToProcess
 import os
 import sqlite3
-import glob
+
 
 def link_db_excel(files):
+    """
+    This function takes a list of files and looks at the location of these files for the excel file with information.
+    It then returns a class list based on the wildtypes and mutants.
+    """
+
     location = os.path.dirname(files[0])
 
     animal_info = []
     for file in files:
-        animal_info.append(db_animals(file))
+        animal_info.append(db_animals(
+            file))  # <- This function returns a 2d list of all animals with their id in the database and their RFID
 
-    overview = read_excel(location).values.tolist()
+    overview = read_excel(
+        location).values.tolist()  # <- This function returns a pandas dataframe with the excel information
 
     WT_HET = []
 
+
+    """
+        The loop here checks the database information against the excel file information and returns a class list.
+        The class list is based on the order of the files.
+        In this list the 0 represents a wildtype and the 1 a mutant.
+    """
     for row in animal_info:
         for animal in row:
             for info in overview:
@@ -28,14 +41,19 @@ def link_db_excel(files):
 
 
 def read_excel(location):
+    """
+    The excel file at the location of the files is read into a pandas dataframe.
+    """
     os.chdir(location)
-    #for file in glob.glob("*.xlsx"):
     location = location + '/' + 'overview_dataset.xlsx'
     xl_file = pd.read_excel(location)
     return xl_file
 
 
 def db_animals(file):
+    """
+    This function returns the id and frid of each animal in the database
+    """
     conn = sqlite3.connect(
         file)  # <- Connect to the database using the variable declared in main
 
@@ -48,34 +66,7 @@ def db_animals(file):
     return results
 
 
-def read_database_info(files):
-    info = {}
-    location = os.path.dirname(files)
-    os.chdir(location)
-    #for file in glob.glob("*.xlsx"):
-    location = location + '/' + 'overview_dataset.xlsx'
-
-    df = pd.read_excel(location)
-    for index, row in df.iterrows():
-        if row[2] == 'WT':
-            row[2] = 0
-        else:
-            row[2] = 1
-
-        if isinstance(row[0], str):
-            fileName = row[0]
-        else:
-            row[0] = fileName
-    for index, row in df.iterrows():
-        if row[0] not in info:
-            info[row[0]] = [[row[1], row[2], row[3]]]
-        else:
-            tempList = info[row[0]]
-            tempList.append([[row[1], row[2], row[3]]])
-            info[row[0]] = tempList
-    return info
-
-
 if __name__ == '__main__':
     files = getFilesToProcess()
     wt_het = link_db_excel(files)
+    print(wt_het)
