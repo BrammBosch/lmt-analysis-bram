361dbb2fcf526a004a82364bb4208c8e5972603a
BrammBosch <31956479+BrammBosch@users.noreply.github.com>
2021-03-29 13:55:01 +0200

    Added a function to cut a pre-merge database on the event table

--- LMT/scripts/tools/cut_frames_timestamps.py
+++ LMT/scripts/tools/cut_frames_timestamps.py
@@ -1,7 +1,7 @@
 import sqlite3
 import datetime
 from lmtanalysis.FileUtil import getFilesToProcess
-from scripts.tools.find_time_frames import find_start_end_file_epoch
+from scripts.tools.find_time_frames import find_start_end_file_epoch, find_start_end_file
 import time
 
 def cut_time_timestamps(file):
@@ -15,9 +15,31 @@ def cut_time_timestamps(file):
     cursor.execute(query, val)
     conn.commit()
 
+def cut_event_table(file):
+    start_frame, end_frame = find_start_end_file(file)
+    print(start_frame,end_frame)
+    listExcludedEvents = ['RFID ASSIGN ANONYMOUS TRACK',
+                          'RFID MATCH',
+                          'RFID MISMATCH',
+                          'MACHINE LEARNING ASSOCIATION',
+                          'Detection',
+                          'Head detected'
+                          ]
+
+    conn = sqlite3.connect(file)  # <- Connect to the database using the variable declared in main
+    cursor = conn.cursor()
+    placeholder = '?'
+    placeholders = ', '.join(placeholder for unused in listExcludedEvents)
+
+    query = 'delete from event where name not in (%s) and (startframe < ? or endframe > ?)' %placeholders
+    listExcludedEvents.append(start_frame)
+    listExcludedEvents.append(end_frame)
+    val = tuple(listExcludedEvents)
+    cursor.execute(query, val)
+    conn.commit()
 
 if __name__ == '__main__':
 
     files = getFilesToProcess()
     for file in files:
-        cut_time_timestamps(file)
+        cut_event_table(file)
